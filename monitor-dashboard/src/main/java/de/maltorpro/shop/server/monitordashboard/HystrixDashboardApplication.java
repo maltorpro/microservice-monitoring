e3ffc79e8906879cb6e14d33a8fcaffaaf96f659
Malte Basse <malte.basse@gebit.de>
2018-05-04 10:29:12 +0200

    Control of the certificate exam

--- monitor-dashboard/src/main/java/de/maltorpro/shop/server/monitordashboard/HystrixDashboardApplication.java
+++ monitor-dashboard/src/main/java/de/maltorpro/shop/server/monitordashboard/HystrixDashboardApplication.java
@@ -1,5 +1,9 @@
 package de.maltorpro.shop.server.monitordashboard;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,6 +11,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import de.maltorpro.shop.monitoring.utils.SSLUtils;
+;
 
 @SpringBootApplication
 @Controller
@@ -23,7 +30,15 @@ public class HystrixDashboardApplication extends SpringBootServletInitializer {
         return application.sources(HystrixDashboardApplication.class).web(WebApplicationType.SERVLET);
     }
 
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(HystrixDashboardApplication.class).web(WebApplicationType.SERVLET).run(args);
+    public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException {
+        
+    	String certificateCheck =  System.getProperty("certificateCheck");
+		
+		if(StringUtils.equals(certificateCheck, "false") || StringUtils.equals(certificateCheck, "0")) {
+			
+			SSLUtils.turnOffSslChecking();
+		}
+    	
+    	new SpringApplicationBuilder(HystrixDashboardApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
---
+++ monitoring-utils/src/main/java/de/maltorpro/shop/monitoring/utils/WhitelistHostnameVerifier.java
@@ -0,0 +1,32 @@
+package de.maltorpro.shop.monitoring.utils;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+import java.util.HashSet;
+import java.util.Set;
+
+// singleton
+public enum WhitelistHostnameVerifier implements HostnameVerifier {
+    // these hosts get whitelisted
+    INSTANCE("localhost");
+
+    private Set<String> whitelist = new HashSet<>();
+    private HostnameVerifier defaultHostnameVerifier =
+            HttpsURLConnection.getDefaultHostnameVerifier();
+
+    WhitelistHostnameVerifier(String... hostnames) {
+        for (String hostname : hostnames) {
+            whitelist.add(hostname);
+        }
+    }
+
+    @Override
+    public boolean verify(String host, SSLSession session) {
+        if (whitelist.contains(host)) {
+            return true;
+        }
+        // important: use default verifier for all other hosts
+        return defaultHostnameVerifier.verify(host, session);
+    }
+}
