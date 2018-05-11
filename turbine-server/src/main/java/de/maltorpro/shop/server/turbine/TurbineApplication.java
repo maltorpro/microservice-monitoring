68c9ad20a6ae00ca487c8e3b8251ad54f21c3264
Malte Basse <maltebasse@gmail.com>
2018-05-11 02:34:03 +0200

    http2 support

--- turbine-server/src/main/java/de/maltorpro/shop/server/turbine/TurbineApplication.java
+++ turbine-server/src/main/java/de/maltorpro/shop/server/turbine/TurbineApplication.java
@@ -6,25 +6,25 @@ import java.security.NoSuchAlgorithmException;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 import de.maltorpro.shop.monitoring.utils.SSLUtils;
 
 @SpringBootApplication
 @EnableTurbine
-@EnableDiscoveryClient
 public class TurbineApplication {
 
-    public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException {
-    	
-	String certificateCheck =  System.getProperty("certificateCheck");
-		
-		if(StringUtils.equals(certificateCheck, "false") || StringUtils.equals(certificateCheck, "0")) {
-			
-			SSLUtils.turnOffSslChecking();
-		}
-    	
-    	SpringApplication.run(TurbineApplication.class,args);
+    public static void main(String[] args)
+            throws KeyManagementException, NoSuchAlgorithmException {
+
+        String certificateCheck = System.getProperty("certificateCheck");
+
+        if (StringUtils.equals(certificateCheck, "false")
+                || StringUtils.equals(certificateCheck, "0")) {
+
+            SSLUtils.turnOffSslChecking();
+        }
+
+        SpringApplication.run(TurbineApplication.class, args);
     }
 }
