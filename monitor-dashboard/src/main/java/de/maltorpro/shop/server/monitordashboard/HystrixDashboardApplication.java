645cc6b3c1eea91c3f4d91e1803a2c8146584817
maltorpro <maltebasse@gmail.com>
2020-01-16 11:48:33 +0100

    Http2 support (#1)
    * changed configuration server label
    
    * Added maltor.de certificate
    
    * Changed urls to https
    
    * Control of the certificate exam
    
    * Control of the certificate exam
    
    * http2 support
    
    * Compile only jar
    
    * Disable certificate check via CERTIFICATE_CHECK
    
    * Changed config server url for docker
    
    * Added logger configuration
    
    * Added docker-swarm to white list
    
    * Changed config server url
    
    * Changed white list to host ip address
    
    * Changed config server url
    
    * Docker container url configuration
    
    * Changed white list
    
    * Updated certificate and dockerfile
    
    * Changed spring cloud verson
    
    * Updated docker files
    
    * Changed spring cloud version
    
    * Updated maltor.de certificate
    
    * moved certificate to utils/main project
    
    * updated sonarqube plugin and gradle version
    
    * add out folder zo gitignore
    
    * updated gradel wrapper
    
    * updated gradel wrapper
    
    * removal of gradle warnings
    
    * update spring
    
    * update spring

--- monitor-dashboard/src/main/java/de/maltorpro/shop/server/monitordashboard/HystrixDashboardApplication.java
+++ monitor-dashboard/src/main/java/de/maltorpro/shop/server/monitordashboard/HystrixDashboardApplication.java
@@ -1,5 +1,9 @@
 package de.maltorpro.shop.server.monitordashboard;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,22 +12,40 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import de.maltorpro.shop.monitoring.utils.SSLUtils;;
+
 @SpringBootApplication
 @Controller
 @EnableHystrixDashboard
 public class HystrixDashboardApplication extends SpringBootServletInitializer {
-	
-	@RequestMapping("/")
-	public String home() {
-		return "forward:/hystrix";
-	}
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(HystrixDashboardApplication.class).web(WebApplicationType.SERVLET);
+    @RequestMapping("/")
+    public String home() {
+        return "forward:/hystrix";
     }
 
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(HystrixDashboardApplication.class).web(WebApplicationType.SERVLET).run(args);
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(HystrixDashboardApplication.class)
+                .web(WebApplicationType.SERVLET);
+    }
+
+    public static void main(String[] args)
+            throws KeyManagementException, NoSuchAlgorithmException {
+
+        String certificateCheckProp = System.getProperty("certificateCheck");
+        String certificateCheckEnv = System.getenv("CERTIFICATE_CHECK");
+
+        if (StringUtils.equals(certificateCheckProp, "false")
+                || StringUtils.equals(certificateCheckProp, "0")
+                || StringUtils.equals(certificateCheckEnv, "false")
+                || StringUtils.equals(certificateCheckEnv, "0")) {
+
+            SSLUtils.turnOffSslChecking();
+        }
+
+        new SpringApplicationBuilder(HystrixDashboardApplication.class)
+                .web(WebApplicationType.SERVLET).run(args);
     }
 }
--- turbine-server/src/main/java/de/maltorpro/shop/server/turbine/TurbineApplication.java
+++ turbine-server/src/main/java/de/maltorpro/shop/server/turbine/TurbineApplication.java
@@ -1,16 +1,33 @@
 package de.maltorpro.shop.server.turbine;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+import de.maltorpro.shop.monitoring.utils.SSLUtils;
 
 @SpringBootApplication
 @EnableTurbine
-@EnableDiscoveryClient
 public class TurbineApplication {
 
-    public static void main(String[] args) {
-    	SpringApplication.run(TurbineApplication.class,args);
+    public static void main(String[] args)
+            throws KeyManagementException, NoSuchAlgorithmException {
+
+        String certificateCheckProp = System.getProperty("certificateCheck");
+        String certificateCheckEnv = System.getenv("CERTIFICATE_CHECK");
+
+        if (StringUtils.equals(certificateCheckProp, "false")
+                || StringUtils.equals(certificateCheckProp, "0")
+                || StringUtils.equals(certificateCheckEnv, "false")
+                || StringUtils.equals(certificateCheckEnv, "0")) {
+
+            SSLUtils.turnOffSslChecking();
+        }
+
+        SpringApplication.run(TurbineApplication.class, args);
     }
 }
